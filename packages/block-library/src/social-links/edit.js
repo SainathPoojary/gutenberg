4c983b396760acede2a50c6fd6e8d01e438a0e02
Sainath Poojary <spoojary614@gmail.com>
2025-01-23 22:05:08 +0530

    Social: Update color label to reflect 'Show text' setting

--- packages/block-library/src/social-links/edit.js
+++ packages/block-library/src/social-links/edit.js
@@ -137,7 +137,7 @@ export function SocialLinksEdit( props ) {
 				setIconColor( colorValue );
 				setAttributes( { iconColorValue: colorValue } );
 			},
-			label: __( 'Icon color' ),
+			label: showLabels ? __( 'Icon & label color' ) : __( 'Icon color' ),
 			resetAllFilter: () => {
 				setIconColor( undefined );
 				setAttributes( { iconColorValue: undefined } );
